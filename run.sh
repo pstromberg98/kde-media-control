54269c644cc7c8a211962c2d8aafc0caff8425c4
Parker Stromberg <parker.stromberg@gmail.com>
2019-06-26 21:47:39 -0600

    Improved install process

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-kpackagetool5 --remove src/package
-kpackagetool5 -t Plasma/Applet --install src/package
+kpackagetool5 -g --remove src/package
+kpackagetool5 -g -t Plasma/Applet --install src/package
 
 plasmoidviewer --applet parker.mediacontrol
 
