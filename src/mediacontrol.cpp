6c944a2a3c7a53974a3224ebd9188990064d4318
Parker Stromberg <parker.stromberg@gmail.com>
2019-07-01 20:23:46 -0600

    Got audio buffer to fft

--- src/mediacontrol.cpp
+++ src/mediacontrol.cpp
@@ -25,48 +25,66 @@ void MediaControl::init() {
 	QAudioRecorder* audioRecorder = new QAudioRecorder;
 
 	QStringList inputs = audioRecorder->audioInputs();
+	qDebug(audioRecorder->defaultAudioInput().toLatin1());
 	QString selectedInput = audioRecorder->defaultAudioInput();
 
 	foreach (QString input, inputs) {
 	    QString description = audioRecorder->audioInputDescription(input);
-	    qDebug(description.toLatin1());
+	    qDebug(input.toLatin1());
 	}
 
-	audioRecorder->setAudioInput(selectedInput);
+	audioRecorder->setAudioInput("alsa:surround21:CARD=Loopback,DEV=0");
 
-	QAudioProbe* audioProbe = new QAudioProbe(this);
-
+	QAudioProbe* audioProbe = new QAudioProbe(this); 
 	if (audioProbe->setSource(audioRecorder)) {
 	    // Probing succeeded, audioProbe->isValid() should be true.
 	    connect(audioProbe, SIGNAL(audioBufferProbed(QAudioBuffer)),
-		    this, SLOT(calculateLevel(QAudioBuffer)));
+		    this, SLOT(calculateFrequency(QAudioBuffer)));
 	}
 
 	audioRecorder->record();
 }
 
 void MediaControl::calculateFrequency(QAudioBuffer buffer) {
-    return;
-    // input signal parameters
-    const std::size_t SIZE = 64;
-    const Aquila::FrequencyType sampleFreq = 2000;
-    const Aquila::FrequencyType f1 = 125, f2 = 700;
+    printf("Frame Count: %u\n", buffer.frameCount());
+    printf("Sample Size: %u\n", buffer.format().sampleSize());
+    printf("Sample Type: %u\n", buffer.format().sampleType());
 
-    Aquila::SineGenerator sineGenerator1 = Aquila::SineGenerator(sampleFreq);
-    sineGenerator1.setAmplitude(32).setFrequency(f1).generate(SIZE);
-    Aquila::SineGenerator sineGenerator2 = Aquila::SineGenerator(sampleFreq);
-    sineGenerator2.setAmplitude(8).setFrequency(f2).setPhase(0.75).generate(SIZE);
-    auto sum = sineGenerator1 + sineGenerator2;
+    if (buffer.isValid()) {
+	    std::vector<double> vdData(buffer.frameCount());
+    	    QAudioBuffer::S16S *data = buffer.data<QAudioBuffer::S16S>();
 
-    Aquila::TextPlot plt("Input signal");
-    plt.plot(sum);
+	    for (int i = 0; i < buffer.frameCount(); i++) {
+		vdData[i] = (double) data[i].average();
+	    }
 
-    // calculate the FFT
-    auto fft = Aquila::FftFactory::getFft(SIZE);
-    Aquila::SpectrumType spectrum = fft->fft(sum.toArray());
 
-    plt.setTitle("Spectrum");
-    plt.plotSpectrum(spectrum);
+	    double dData[vdData.size()];
+
+	    std::copy(vdData.begin(), vdData.end(), dData);
+
+    	    printf("Data Size: %u\n", buffer.format().sampleType());
+
+	    Aquila::TextPlot plt("Input signal");
+	    // plt.plot(data, buffer.frameCount());
+	    // calculate the FF
+	    auto fft = Aquila::FftFactory::getFft(256);
+	    Aquila::SpectrumType spectrum = fft->fft(dData);
+
+	    printf("Spectrum Size: %u", spectrum.size());
+	    plt.setTitle("Spectrum");
+	    plt.plotSpectrum(spectrum);
+
+    	    int halfLength = spectrum.size() / 2;
+	    std::vector<double> absSpectrum(halfLength);
+     	    for (int i = 0; i < absSpectrum.size(); ++i)
+            {
+            	absSpectrum[i] = std::abs(spectrum[i]);//returns the magnitude of a complex number
+            }
+
+	    printf("Spectrum Size: %u", absSpectrum.size());
+    }
+
 }
 
 
